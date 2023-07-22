d3cf33e14f5eaf94ace7e2f574efa8df1a786050
Chekos10 <119345370+Chekos10@users.noreply.github.com>
2023-07-22 22:53:59 +0300

    save_third

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import { Button } from "./Button/Button";
 import { ImageGallery } from "./ImageGallery/ImageGallery";
 import { Loader } from "./Loader/Loader";
 import { Searchbar } from "./Searchbar/Searchbar";
@@ -13,19 +14,27 @@ export class App extends Component {
   }
 
   hendleSearch=(searchImg)=>{
-    this.setState({searchImg})
+    this.setState({searchImg , page:1, images:[]})
+  }
+  onMoreBtnClick = () =>{
+    this.setState(prevState =>({
+      page:prevState.page+1
+    }))
   }
     componentDidUpdate(prevProps, prevState) {
-    if (prevState.searchImg!== this.state.searchImg) {
-        if(this.state.images.length === 0){
+    
+    if (prevState.searchImg!== this.state.searchImg || prevState.page!==this.state.page) {
+        const {searchImg,page,images}= this.state
+        if(images.length === 0 || page ===1){
           this.setState({isLoading:true})
         }
-        getImg(this.state.searchImg)
+        getImg(searchImg,page)
         .then(images => {
           const { data } = images;
-          this.setState({ images: [...data.hits] });
+          this.setState({ 
+          images: [...prevState.images, ...data.hits],
+          isLoading: false,});
         })
-
         .catch((error)=>{
           this.setState({error})
         })
@@ -35,11 +44,12 @@ export class App extends Component {
     }
   }
   render(){
-    const {isLoading} = this.state
+    const {isLoading,images} = this.state
     return (
       <>
         <Searchbar hendleSearch = {this.hendleSearch}/>
         <ImageGallery data = {this.state.images}/>
+        {images.length >0 && <Button onMoreBtnClick = {this.onMoreBtnClick}/>}
         {isLoading && <Loader/>}
       </>
       
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,7 @@
+export const Button = ({onMoreBtnClick}) => {
+    return (
+        <button className="LoadMoreBtn" onClick={onMoreBtnClick}>
+        Load more
+        </button>
+    )
+}
